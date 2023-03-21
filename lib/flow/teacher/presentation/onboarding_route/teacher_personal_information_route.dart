9992b4be421cec69d8dfc8caccdc802ff84d3309
naman <naman@newversity.in>
2023-03-21 17:30:41 +0530

    teacher personal information route

--- lib/flow/teacher/presentation/onboarding_route/teacher_personal_information_route.dart
+++ lib/flow/teacher/presentation/onboarding_route/teacher_personal_information_route.dart
@@ -43,7 +43,7 @@ class _TeacherPersonalInfoRouteState extends State<TeacherPersonalInfoRoute> {
           return getContentWidget(false);
         }, listener: (BuildContext context, TeacherDetailsState state) {
           if (state is TeacherDetailsSavingSuccessState) {
-            Navigator.of(context).pushNamed(AppRoutes.studentHome);
+            Navigator.of(context).pushNamed(AppRoutes.teacherExperienceAndQualificationRoute);
           } else if (state is TeacherDetailsSavingFailureState) {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
